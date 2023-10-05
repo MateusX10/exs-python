e33642fed154e9f5b87b87671ee1a2c41223ec61
Mateus Medeiros <50000060+MateusX10@users.noreply.github.com>
2023-10-05 14:24:34 -0300

    arquivo "main.py" adicionado

---
+++ revisao/strings/ex2/main.py
@@ -0,0 +1,8 @@
+from strings import leiaString, contaVogais
+
+
+string = leiaString("Informe uma string: ")
+
+numeroVogais = contaVogais(string)
+
+print(f"String = {string}\nNúmero de vogais = {numeroVogais}")
