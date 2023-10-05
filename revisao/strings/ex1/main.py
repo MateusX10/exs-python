1fa215195c63c75d6ef5bff4ba8901133fcbfd18
Mateus Medeiros <50000060+MateusX10@users.noreply.github.com>
2023-10-05 14:20:14 -0300

    Create main.py

---
+++ revisao/strings/ex1/main.py
@@ -0,0 +1,10 @@
+from strings import leiaString
+        
+
+string = leiaString("Digite uma string: ")
+
+
+# inverte string
+stringInvertida = string[::-1]
+
+print(f"String = {string} \nString invertida = {stringInvertida}")
